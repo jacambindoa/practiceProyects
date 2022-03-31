2edc92324b5ef16e877fc33ac8674d1ae15dbf83
Jonathan <jcambindoarango@gmail.com>
2022-03-30 22:42:43 -0500

    Encapsulando codigo en funciones

--- Javascript/geometricsFigures/script.js
+++ Javascript/geometricsFigures/script.js
@@ -1,15 +1,22 @@
 //Codigo cuadrado
 console.group("Cuadrados");
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: "+ladoCuadrado+" cm");
-const perimetroCuadrado = ladoCuadrado*4;
-console.log("El perimetro del cuadrado es: "+perimetroCuadrado+" cm");
-const AreaCuadrado = ladoCuadrado*ladoCuadrado;
-console.log("El área del cuadrado mide "+AreaCuadrado+" cm^2");
+/* const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: "+ladoCuadrado+" cm"); */
+
+function perimetroCuadrado (lado){
+    return lado*4;
+} 
+
+function AreaCuadrado (lado) {
+    return lado*lado;
+}
+
+//console.log("El perimetro del cuadrado es: "+perimetroCuadrado+" cm");
+//console.log("El área del cuadrado mide "+AreaCuadrado+" cm^2");
 console.groupEnd();
 //Codigo triangulo
 console.group("Triangulos");
-const ladoTriangulo1 = 6;
+/* const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
 const alturaTriangulo = 5.5;
@@ -19,19 +26,36 @@ console.log(
     +ladoTriangulo2+" cm, "
     +baseTriangulo+" cm."
     +" La altura del triangulo es: "+alturaTriangulo+" cm."
-    );
-const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+baseTriangulo;
-console.log("El perimetro del triangulo es: "+perimetroTriangulo+" cm");
-const areaTriangulo =(baseTriangulo*alturaTriangulo)/2;
-console.log("El area del triangulo es: "+areaTriangulo+" cm^2.");
+    ); */
+function perimetroTriangulo (lado1, lado2, base){
+    return lado1+lado2+base;
+} 
+
+//ladoTriangulo1+ladoTriangulo2+baseTriangulo;
+//console.log("El perimetro del triangulo es: "+perimetroTriangulo+" cm");
+
+function areaTriangulo (baseTriangulo, alturaTriangulo){
+    return(baseTriangulo*alturaTriangulo)/2;
+} 
+
+//console.log("El area del triangulo es: "+areaTriangulo+" cm^2.");
 console.groupEnd(); 
 //Codigo circulo
 console.group("Circulo");
-const radioCirculo = 4;
-const diametroCirculo = radioCirculo*2;
 const pi=Math.PI;
-const perimetroCirculo= diametroCirculo*pi;
-const areaCirculo=(radioCirculo*radioCirculo)*pi;
+//const radioCirculo = 4;
+function diametroCirculo (radio){
+    return radio*2;
+}
+function perimetroCirculo(radio){
+    const diametro=diametroCirculo(radio);
+    return diametro*pi; 
+}
+
+function areaCirculo(radio){
+    retur (radio*radio)*PI;
+}
+
 console.log("El numero pi equivale a: "+pi);
 console.log("El perimetro del circulo es: "+perimetroCirculo+" cm.");
 console.log("El area del circulo es: "+areaCirculo+" cm^2.");
