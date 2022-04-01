5e5efe01d570e0f5074cb5c073272e8a68c9fee7
Jonathan <jcambindoarango@gmail.com>
2022-03-31 23:26:17 -0500

    Incorporacion de formularios e interaccion con Javascript desde html

--- Javascript/geometricsFigures/script.js
+++ Javascript/geometricsFigures/script.js
@@ -7,7 +7,7 @@ function perimetroCuadrado (lado){
     return lado*4;
 } 
 
-function AreaCuadrado (lado) {
+function areaCuadrado (lado) {
     return lado*lado;
 }
 
@@ -61,4 +61,26 @@ console.log("El perimetro del circulo es: "+perimetroCirculo+" cm.");
 console.log("El area del circulo es: "+areaCirculo+" cm^2.");
 console.groupEnd(); 
 
-    
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert (perimetro+" cm")
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert (area+" cm2");
+
+}
+function calcularAreaTriangulo(){
+    const input1 = document.getElementById("inputAlturaTriangulo");
+    const input2 = document.getElementById("inputBaseTriangulo");
+    const value1 = input1.value;
+    const value2 = input2.value;
+    const area1 = areaTriangulo(value1,value2);
+    alert (area1+" cm2");
+
+}
