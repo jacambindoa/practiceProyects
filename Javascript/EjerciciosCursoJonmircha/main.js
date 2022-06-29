86926a88bdf164e1d38e5fc1691b5388fb971d33
Jonathan <jcambindoarango@gmail.com>
2022-06-29 05:19:20 -0500

    Solucion hasta el ejercicio 26

--- Javascript/EjerciciosCursoJonmircha/main.js
+++ Javascript/EjerciciosCursoJonmircha/main.js
@@ -375,12 +375,147 @@ const validarCorreo = (email="") => {
 
 /* 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25]. */
 
+const numerosAlCuadrado = (arr = undefined) => {
+  if (arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+  if (!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+  if (arr.length === 0) return console.error ("El arreglo esta vacío");
+  for (let num of arr){
+    if(typeof num !=="number") return console.error(`El valor "${num}", no es un numero`)
+  }
+  const newArr = arr.map(el => el*el);
 
+  return console.info (`Arreglo original ${arr}, \nArreglo elevado al cuadrado ${newArr}`);
+}
 
+// numerosAlCuadrado ();
+// numerosAlCuadrado (true);
+// numerosAlCuadrado ({});
+// numerosAlCuadrado ([]);
+// numerosAlCuadrado ([1, "3", 5]);
+// numerosAlCuadrado ([1, 3, 5]);
 
 /* 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60]. */
 
+const numeroAltoBajo = (arr = undefined) => {
+  if (arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+  if (!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+  if (arr.length === 0) return console.error ("El arreglo esta vacío");
+  for (let num of arr){
+    if(typeof num !=="number") return console.error(`El valor "${num}", no es un numero`)
+  }
 
+  return console.info(`Arreglo original: ${arr}\nValor mayor: ${Math.max(...arr)}, \nValor menor: ${Math.min(...arr)}`)
+}
 
+// numeroAltoBajo();
+// numeroAltoBajo(false);
+// numeroAltoBajo([]);
+// numeroAltoBajo(["55"]);
+// numeroAltoBajo([55, 42, 21,77, 9]);
 
 /* 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}. */
+
+const separarParImpar = (arr = undefined) => {
+  if (arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+  if (!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+  if (arr.length === 0) return console.error ("El arreglo esta vacío");
+  for (let num of arr){
+    if(typeof num !=="number") return console.error(`El valor "${num}", no es un numero`);
+  }
+
+  return console.info({
+    pares:arr.filter(num => num%2===0),
+    impares:arr.filter(num => num%2===1)
+  })
+}
+
+// separarParImpar([1,2,3,4,5,6,7,8,9,10,11,12,13]);
+
+/* 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }. */
+
+const arregloAsendenteDesendente = (arr = undefined) => {
+  if (arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+  if (!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+  if (arr.length === 0) return console.error ("El arreglo esta vacío");
+  for (let num of arr){
+    if(typeof num !=="number") return console.error(`El valor "${num}", no es un numero`);
+  }
+
+  return console.info({
+    arr,
+    asc: arr.map(el=>el).sort(),
+    desc: arr.map(el=>el).sort().reverse()
+  });
+}
+
+// arregloAsendenteDesendente([4,6,75,43,58,2,9,32]) NO DIO
+
+/* 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true]. */
+
+const borrarRepetidos = (arr=undefined) => {
+  if (arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+  if (!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+  if (arr.length === 0) return console.error ("El arreglo esta vacío");
+
+  /* return console.info({
+    original: arr,
+    sinRepetidos: arr.filter((value, index, self) => self.indexOf(value) === index)
+  }) */
+
+  return console.info({
+    original: arr,
+    sinRepetidos:[...new Set(arr)]
+  })
+
+}
+
+// borrarRepetidos(["x",10,true,"2",3,"Hola","x",true])
+
+/* 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5. */
+
+const promedio = (arr= undefined) => {
+  if (arr === undefined) return console.warn("No ingresaste un arreglo de numeros");
+  if (!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+  if (arr.length === 0) return console.error ("El arreglo esta vacío");
+  for (let num of arr){
+    if(typeof num !=="number") return console.error(`El valor "${num}", no es un numero`);
+  }
+
+  return console.info(
+    arr.reduce((total, num, index, arr) => {
+      total += num
+      if(index===arr.length-1){
+        return `El promedio de ${arr.join(" + ")} es ${total / arr.length}`;
+      }else{
+        return total;
+      }
+    })
+  )
+}
+
+// promedio();
+// promedio([1,2,3,4,5,6,7,8,9,0]);
+
+/* 27) Programa una clase llamada Pelicula.
+La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
+  - Todos los datos del objeto son obligatorios.
+  - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
+    7 restantes números.
+  - Valida que el título no rebase los 100 caracteres.
+  - Valida que el director no rebase los 50 caracteres.
+  - Valida que el año de estreno sea un número entero de 4 dígitos.
+  - Valida que el país o paises sea introducidos en forma de arreglo.
+  - Valida que los géneros sean introducidos en forma de arreglo.
+  - Valida que los géneros introducidos esten dentro de los géneros 
+    aceptados*.
+  - Crea un método estático que devuelva los géneros aceptados*.
+  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
+    decimal de una posición.
+  - Crea un método que devuelva toda la ficha técnica de la película.
+  - Apartir de un arreglo con la información de 3 películas genera 3 
+    instancias de la clase de forma automatizada e imprime la ficha técnica 
+    de cada película.
+
+* Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western. */
+
+
