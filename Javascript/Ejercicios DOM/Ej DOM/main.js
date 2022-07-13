b68cfd57c76bc01f71381db62701d6b1fc010c3a
Jonathan <jcambindoarango@gmail.com>
2022-07-13 18:41:04 -0500

    Implementacion reloj digital

--- Javascript/Ejercicios DOM/Ej DOM/main.js
+++ Javascript/Ejercicios DOM/Ej DOM/main.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
+import { digitalClock, alarm } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu(".panel-btn",".panel",".menu a");
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
 })
