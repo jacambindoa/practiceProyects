4730d8d7d958f54d9396b562ce2129d9fc45414c
Jonathan <jcambindoarango@gmail.com>
2022-07-17 09:11:36 -0500

    Implementacion cuenta regresiva

--- Javascript/Ejercicios DOM/Ej DOM/main.js
+++ Javascript/Ejercicios DOM/Ej DOM/main.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall } from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,8 +9,10 @@ d.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu(".panel-btn",".panel",".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/Alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Jul 17, 2022 09:06:00", "Feliz Cumpleaños programador");
 })
 
 d.addEventListener("keydown", e => {
   moveBall(e, ".ball", ".stage");
 })
+
