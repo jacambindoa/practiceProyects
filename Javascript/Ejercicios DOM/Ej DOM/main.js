a4db0ec26ac6188b4e5cd74f875ca9666df9d61d
Jonathan <jcambindoarango@gmail.com>
2022-07-23 21:22:23 -0500

    Implementacion dark mode 2

--- Javascript/Ejercicios DOM/Ej DOM/main.js
+++ Javascript/Ejercicios DOM/Ej DOM/main.js
@@ -2,6 +2,8 @@ import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall } from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
+import scrollTopButton from "./boton_scroll.js"
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
@@ -10,9 +12,11 @@ d.addEventListener("DOMContentLoaded", e => {
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/Alarma.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "Jul 17, 2022 09:06:00", "Feliz Cumpleaños programador");
+  scrollTopButton(".scroll-top-btn");
 })
 
 d.addEventListener("keydown", e => {
   moveBall(e, ".ball", ".stage");
 })
 
+darkTheme(".dark-theme-btn", "dark-mode");
